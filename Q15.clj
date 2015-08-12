67ca325de4569aab9ff0886b5d9ffc6cf464db73
doyougnu <jmy6342@gmail.com>
2015-08-12 16:29:12 -0500

    Q15

---
+++ Q15.clj
@@ -0,0 +1,16 @@
+;; 4Clojure Question 15
+;;
+;; Write a function which doubles a number.
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+
+(defn dub [x]
+  (* x 2))
+
+(= (dub 2) 4)
+
+(= (dub 3) 6)
+
+(= (dub 11) 22)
+
+(= (dub 7) 14)
