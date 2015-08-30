94ad929399d33b26197752540da035f84bda45f8
doyougnu <jmy6342@gmail.com>
2015-08-30 12:49:39 -0500

    Q77

--- Q77.clj
+++ Q77.clj
@@ -4,23 +4,44 @@
 ;;
 ;; Use M-x 4clojure-check-answers when you're done!
 
+(defn inVector? [elem vect]
+  (mapv (fn [subset] some #(= elem %) subset) vect))
+
+(defn intersect-vector [x y]
+  (loop [x (seq x) y (seq y) acc []]
+    (if (and x y)
+      (let [x1 (first x) ]
+      (cond
+       (inVector? x1 y) (recur (next x) y (conj acc x1))
+       :else (recur (next x) y acc)))
+    acc)))
+
 (defn toCharArray [s]
-  (seq (char-array s)))
+  (vec (char-array s)))
 
 (defn toString [ca]
   (apply str ca))
 
 (defn anagramList [word]
-  (distinct (take 100 (iterate shuffle word))))
+  (into [] (take 50 (drop 1 (iterate shuffle word)))))
 
 (defn anagrams [coll]
-  (let [coll (set (map #(set toCharArray %) coll))
-        collset (into #{} coll)
-        anagramset (into #{} (map #(anagramList %) coll))]
-    (print collset)))
+  (let [colls (mapv toCharArray coll)
+        anagrams (mapv #(anagramList %) colls)]
+   (pprint anagrams)))
 
-(= (__ ["meat" "mat" "team" "mate" "eat"])
+;;;after much struggling with trying to generate an anagram list, then functionally match the anagrams, yyhh's solution is much cheaper and elegant focusing on distribution of characters insead.
+
+(defn anagrams2 [coll]
+  (->> (group-by frequencies coll)
+       (vals)
+       (filter #(> (count %) 1))
+       (map set)
+       set))
+
+
+(= (anagrams2 ["meat" "mat" "team" "mate" "eat"])
    #{#{"meat" "team" "mate"}})
 
-(= (__ ["veer" "lake" "item" "kale" "mite" "ever"])
+(= (anagrams2 ["veer" "lake" "item" "kale" "mite" "ever"])
    #{#{"veer" "ever"} #{"lake" "kale"} #{"mite" "item"}})
